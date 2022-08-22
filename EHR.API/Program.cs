1d4f2fc4f766ab5262bf40a20744761131ce0091
LeoZhou <leo604leo604@gmail.com>
2022-08-23 00:38:28 +0800

    - create project dependcy - use API CORS

--- EHR.API/Program.cs
+++ EHR.API/Program.cs
@@ -1,11 +1,29 @@
+using EHR.Application;
+using EHR.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Module Service
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Allow Browser Call APIS
+var corsPolicy = "APICors";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: corsPolicy,
+                      builder =>
+                      {
+                          builder
+                            .AllowAnyMethod()
+                            .AllowAnyHeader()
+                            .SetIsOriginAllowed(orign => true)
+                            .AllowCredentials();
+                      });
+});
 
 var app = builder.Build();
 
