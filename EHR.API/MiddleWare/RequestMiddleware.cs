ba49afcaf77120418c73ad0fe483e2f286c7a393
LeoZhou <LeoZhou1024@github.com>
2022-09-06 04:20:53 +0800

    setting RequestMiddleware to record log and develop token role

--- EHR.API/MiddleWare/RequestMiddleware.cs
+++ EHR.API/MiddleWare/RequestMiddleware.cs
@@ -16,38 +16,38 @@ namespace EHR.API.MiddleWare
         public async Task Invoke(HttpContext context)
         {
 
-            //var queryPath = context.Request.Path.Value;
-            //if(!queryPath.Contains("swagger"))
-            //{
+            var queryPath = context.Request.Path.Value;
+            if (!queryPath.Contains("swagger"))
+            {
 
-            //    // 確保 HTTP Request 可以多次讀取
-            //    context.Request.EnableBuffering();
+                // makesure HTTP Request can be read more
+                context.Request.EnableBuffering();
 
-            //    // 讀取 HTTP Request Body 內容
-            //    // 注意！要設定 leaveOpen 屬性為 true 使 StreamReader 關閉時，HTTP Request 的 Stream 不會跟著關閉
-            //    using (var bodyReader = new StreamReader(stream: context.Request.Body,
-            //                                              encoding: Encoding.UTF8,
-            //                                              detectEncodingFromByteOrderMarks: false,
-            //                                              bufferSize: 1024,
-            //                                              leaveOpen: true))
-            //    {
-            //        var body = await bodyReader.ReadToEndAsync();
-            //        var log = $"{context.Request.Path}, {context.Request.Method}, {body}";
-            //        Console.WriteLine(log);
-            //    }
+                // read HTTP Request Body 
+                // set  leaveOpen = true. When StreamReader is close,HTTP Request Stream won't close together
+                using (var bodyReader = new StreamReader(stream: context.Request.Body,
+                                                          encoding: Encoding.UTF8,
+                                                          detectEncodingFromByteOrderMarks: false,
+                                                          bufferSize: 1024,
+                                                          leaveOpen: true))
+                {
+                    var body = await bodyReader.ReadToEndAsync();
+                    var log = $"{context.Request.Path}, {context.Request.Method}, {body}";
+                    Console.WriteLine(log);
+                }
 
-            //    // 將 HTTP Request 的 Stream 起始位置歸零
-            //    context.Request.Body.Position = 0;
+                // set HTTP Request Stream position = 0 , let can be read again
+                context.Request.Body.Position = 0;
 
-            await _next.Invoke(context);
+                await _next.Invoke(context);
 
-            //}
-            //else
-            //{
-            //    await _next(context);
-            //}
+            }
+            else
+            {
+                await _next(context);
+            }
 
 
         }
-    }
+        }
 }
